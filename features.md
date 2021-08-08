a740ab7f8551278b9b7ce7711077c7cda77ff4ca
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-08-08 17:35:28 +0100

    Adding RotaBot struct

--- features.md
+++ features.md
@@ -1,3 +1,14 @@
 * able to automatically rotate users from a yaml file on a daily basis
-* way to override rota: try `/rotabot override` => brings up modal
+* way to override rota: try `/rotaboton override` => brings up modal
 * way to manage multiple rotas
+* `/rotaboton set @user` => set the @prodengsupport user to this value
+
+## Commands to implement
+
+* `/rotabot me` => gives you when you're scheduled in the future
+* `/rotabot @user` => gives you when user is scheduled in the future
+* `/rotabot schedule @user 2021-08-01` => schedule user for specific date
+* `/rotatbot schedule all random`
+* `/rotatbot schedule all even`
+* `/rotabot stop`
+* `/rotabot start`
--- main.go
+++ main.go
@@ -1,108 +1,64 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-	"os"
+	"net/http"
 
-	cron "github.com/robfig/cron/v3"
 	"github.com/slack-go/slack"
-	yaml "gopkg.in/yaml.v3"
 )
 
-const slackClient = slack.New(getEnvWithError("ROTABOT_SLACK_TOKEN"), slack.OptionDebug(getEnvWithDefault("ROTABOT_DEBUG", "0")))
-const userGroup = getEnvWithError("ROTABOT_USERGROUP")
+var slackDebug bool
 
-type Rota struct {
-	Users []string `yaml:users`
-}
-
-func check(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
-
-func getEnvWithDefault(env string, def string) string {
-	envValue, ok := os.LookupEnv(env)
-
-	if !ok {
-		envValue = def
-	}
-
-	return envValue
-}
-
-func getEnvWithError(env string) string {
-	envValue, ok := os.LookupEnv(env)
-
-	if !ok {
-		log.Fatalf("Missing required environment variable: %s", env)
-	}
-
-	return envValue
-}
-
-func seekNextUser(email string, users []string) string {
-	totalUsers := len(users)
-
-	for i, userEmail := range users {
-		if userEmail == email {
-			// get the next user in the list - wrap around if the current user is that last user in the list
-			return users[(i+1)%totalUsers]
-		}
-	}
-
-	// if the current user is not found, will start from top of the list
-	return users[0]
-}
-
-func rotate() {
-	var rota Rota
-
-	//go:embed rota.yaml
-	var inRota []byte
-
-	err := yaml.Unmarshal(inRota, &rota)
-	check(err, "Could not unmarshal rota.yaml")
-
-	groupMembers, err := slackClient.GetUserGroupMembers(userGroup)
-
-	if err != nil {
-		// List of Slack error responses: https://api.slack.com/methods/usergroups.users.list
-		switch {
-		case err.Error() == "no_such_subteam":
-			// TODO[1]: finish create usergroup functionality (need function to create the usergroup)
-			slackClient.CreateUserGroup(userGroup)
-		default:
-			check(err, "Could not get group members")
-		}
-	}
-
-	var todaySupportUserEmail string
-
-	// if @prodeng-support not set or has more than 1 member, rotaboton will set the group to the first user in rota.yaml; otherwise
-	// it will start the rotation from the currently set user (if that currently set user is not in the rota list, the rotiation will
-	// start from the first user in rota.yaml)
-	if len(groupMembers) != 1 {
-		todaySupportUserEmail = rota.Users[0]
+func init() {
+	if getEnvWithDefault("ROTABOT_DEBUG", "0") == "0" {
+		slackDebug = false
 	} else {
-		user, err := slackClient.GetUserInfo(groupMembers)
-
-		check(err, "Could not get user information")
-
-		userEmail := user.Profile.Email
-
-		todaySupportUserEmail = seekNextUser(userEmail, rota.Users)
+		slackDebug = true
 	}
 
-	todaySupportUserName, err := slackClient.GetUserByEmail(todaySupportUserEmail)
-	check(err, "Could not get user by email")
+}
 
-	slackClient.UpdateUserGroupMembers(userGroup, todaySupportUserName)
+var slackToken string = getEnvWithError("ROTABOT_SLACK_TOKEN")
+var slackClient *slack.Client = slack.New(slackToken, slack.OptionDebug(slackDebug), slack.OptionLog(&log.Logger{}))
+var rotaBot *RotaBot = NewRotaBot()
+
+func handleEmptySubcommand(w http.ResponseWriter, _ *Command) {
+	msg := NewSlackMsg()
+
+	msg.Text = fmt.Sprintf("Valid subcommands: %s", rotaBot.ListValidCommands())
+
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+}
+
+func handleDefault(w http.ResponseWriter, p *Command) {
+	msg := NewSlackMsg()
+
+	msg.Text = fmt.Sprintf("Invalid subcommand: %s. Valid subcommands: %s", p.Subcommand, rotaBot.ListValidCommands())
+
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+}
+
+func slashHandler(w http.ResponseWriter, r *http.Request) {
+	s, err := slack.SlashCommandParse(r)
+	check(err, "Could not parse Slash command")
+
+	p, err := parse(s.Command, s.Text)
+	check(err, "Could not parse Slash command")
+
+	switch p.Subcommand {
+	case "":
+		handleEmptySubcommand(w, p)
+	default:
+		handleDefault(w, p)
+	}
 }
 
 func main() {
-	scheduler := cron.New()
+	http.HandleFunc("/", slashHandler)
 
-	scheduler.AddFunc("0 0 0 * * 1-5", rotate)
+	http.ListenAndServe(":8080", nil)
 }
